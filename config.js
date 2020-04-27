d27731ff1bb4de29afc68bca010981f670535938
AnnaWijetunga <annawijetunga@gmail.com>
2020-04-27 08:44:07 -0400

    Update posts per page to 3 vs 4

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   subtitle: 'Software Engineer',
   copyright: '© 2020 All rights reserved.',
   disqusShortname: '',
-  postsPerPage: 4,
+  postsPerPage: 3,
   googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
